41bbff3f8a14b5ac51e457018a3678fd443ae714
jodosjodos <jeandedieu2030@gmail.com>
2024-04-25 11:35:34 +0200

    [add] done  with currency convert and update ui

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -3,8 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+  TextEditingController txControl = TextEditingController();
+  double convertedRwf = 0;
+  handleConverter() {
+    double amount = double.tryParse(txControl.text) ?? 0;
+    double rate = 1288.55;
+    setState(() {
+      convertedRwf = amount * rate;
+    });
+  }
 
   @override
   // BuildContext let your widget be know is available
@@ -34,16 +51,31 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              "0",
-              style: TextStyle(
-                  fontSize: 55,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 255, 255, 255)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Text(
+                  "${convertedRwf.toStringAsFixed(2)} ",
+                  style: const TextStyle(
+                      fontSize: 55,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+                const Text(
+                  "RWF",
+                  style: TextStyle(
+                    fontSize: 10,
+                    color: Colors.white,
+                  ),
+                )
+              ],
             ),
             Padding(
               padding: const EdgeInsets.all(10),
               child: TextField(
+                controller: txControl,
                 style: const TextStyle(
                   color: Colors.black,
                 ),
@@ -70,11 +102,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(10),
               child: TextButton(
-                onPressed: () {
-                  if (kDebugMode) {
-                    print("button cocked");
-                  }
-                },
+                onPressed: handleConverter,
                 style: TextButton.styleFrom(
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
