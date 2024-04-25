1959c99a650f31c5a08fbb3d8355a4726d91304b
jodosjodos <jeandedieu2030@gmail.com>
2024-04-25 07:25:13 +0200

    [add] customize textField

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,6 +6,15 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   // BuildContext let your widget be know is available
   Widget build(BuildContext context) {
+    // custom border
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
@@ -17,11 +26,11 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
         backgroundColor: Colors.blue[500],
         centerTitle: true,
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               "0",
               style: TextStyle(
                   fontSize: 55,
@@ -29,20 +38,23 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                   color: Color.fromARGB(255, 255, 255, 255)),
             ),
             TextField(
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
               ),
               decoration: InputDecoration(
-                  hintText: "please enter the amount in  USD",
-                  hintStyle: TextStyle(
-                    color: Colors.black,
-                  ),
-                  prefixIcon: Icon(
-                    Icons.monetization_on_outlined,
-                  ),
-                  prefixIconColor: Colors.black,
-                  filled: true,
-                  fillColor: Colors.white),
+                hintText: "please enter the amount in  USD",
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: const Icon(
+                  Icons.monetization_on_outlined,
+                ),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
             ),
           ],
         ),
