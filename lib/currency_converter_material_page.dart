9ca4afe8f9f7b4b2f439a32628888a242120868e
jodosjodos <jeandedieu2030@gmail.com>
2024-04-25 06:32:04 +0200

    [add] add TextField

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -4,8 +4,10 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
 
   @override
+  // BuildContext let your widget be know is available
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: const Text(
           "Currency Converter",
@@ -16,11 +18,37 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: const Center(
-        child: Text("jdoos gang"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255)),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                  hintText: "please enter the amount in  USD",
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.monetization_on_outlined,
+                  ),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: const Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      
         children: [Text("home"), Text("not"), Text("setting")],
       ),
     );
